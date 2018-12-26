28777a5fab32b3366abdea7c2ea67c6424989834
Sam Kamenetz <samkamenetz@gmail.com>
2018-12-26 18:05:06 -0500

    fleshed out parsing for nick-name and group-name changes, began developing unit tests for Message class

--- message_repackage.py
+++ message_repackage.py
@@ -5,15 +5,49 @@ import time
 class Message:
     # class attribute
     date_format = "%b %d, %Y %I:%M%p"
+    group_change_sig = "named the group "
+    name_change_sig = "set the nickname for"
     def __init__(self, timestamp, contents, author):
         self.timestamp = time.strptime(str, Message.date_format)
         self.timestamp_cmp = time.mktime(self.timestamp)
         self.contents = contents
         self.author = author
+        # 0 for no, 1 for nickname, 2 for group name
+        if Message.group_change_sig in self.contents:
+            self.special = 2
+        elif Message.name_change_sig in self.contents:
+            self.special = 1
+        else:
+            self.special = 0
 
+    def extract_name_change(self):
+        if self.special == 0:
+            return None
+        elif self.special == 1:
+            temp = self.contents.split(Message.name_change_sig)
+            name, nickname = temp[1].split(" to ")
+            nickname = nickname.rstrip(".")
+        elif self.special == 2:
+            temp = self.contents.split(Message.group_change_sig)
+            nickname = temp[1].rstrip(".")
+            name = "group"
+        else:
+            raise ValueError
+        return (nickname, name)
+#testcase 1
 
-#test
+test_arr = [
+    ("Nov 29, 2017 9:19am",
+     "Aanthruuk the scaly Scotsman set the nickname for Joseph G to Reverend Spicy-hands.",
+     "Mike G",
+     ("Joseph G","Reverend Spicy-hands")
+     )
+]
+
+for i in test_arr:
+    x = Message(i[0],i[1],i[2])
+
 str = "Nov 29, 2017 9:19am"
-x = datetime.datetime.strptime(str)
+
 time.strptime(str,"%b %d, %Y %I:%M%p")
 y = time.mktime(x)
