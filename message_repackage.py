d56493490d9274acf9ee51e78f41781c0c5f436c
Sam Kamenetz <samkamenetz@gmail.com>
2018-12-26 17:33:18 -0500

    created logic for parsing time stamp into object using built-in time library

--- message_repackage.py
+++ message_repackage.py
@@ -1,9 +1,19 @@
+import datetime
+import time
 #defines message objects to assist with parsing
 
 class Message:
     # class attribute
-    date_parser = None
+    date_format = "%b %d, %Y %I:%M%p"
     def __init__(self, timestamp, contents, author):
-        self.timestamp = timestamp
+        self.timestamp = time.strptime(str, Message.date_format)
+        self.timestamp_cmp = time.mktime(self.timestamp)
         self.contents = contents
         self.author = author
+
+
+#test
+str = "Nov 29, 2017 9:19am"
+x = datetime.datetime.strptime(str)
+time.strptime(str,"%b %d, %Y %I:%M%p")
+y = time.mktime(x)
